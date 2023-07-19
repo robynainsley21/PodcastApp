9a20397262d979902491dcd06f36d35a4bcadaba
robynainsley21 <robynainsley2i@gmail.com>
2023-07-19 12:17:18 +0200

    Added file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <>
+            <div className='nav'>
+                <img className='logo' src="podcast.png" alt="logo-image" />
+                <img className='settings' src="settings.png" alt="settings-image" />
+            </div>
+            <div className='logo-name'>
+                <h1>Podify</h1>
+            </div>
+        </>
+       
+    )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-    return (
-        <h1 className='nav'>Navbar</h1>
-    )
-}
-
-export default Navbar
