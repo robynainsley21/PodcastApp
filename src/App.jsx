8452c7f6b8438dd94cbeea692f5ddcbd15e44188
robynainsley21 <robynainsley2i@gmail.com>
2023-11-16 16:36:34 +0200

    Rearrange routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import "./index.css";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route  path="/" element={<Home />}>
+      <Route path="signIn" element={<SignIn />} />
+      <Route path="signUp" element={<SignUp />} />
+    </Route>
+  )
+);
+
 const App = () => {
   /* The App function the `useState` hook to create a state variable called `token` and a function
   called `setToken` to update the value of `token` for the sign in page. The initial value of 
   `token` is set to `false`. */
-  const [token, setToken] = useState(false);
+  // const [token, setToken] = useState(false);
 
   //If the token is true, it is stored in sessionStorage with the key 'token'
-  if (token) {
-    sessionStorage.setItem("token", JSON.stringify(token));
-  }
+  // if (token) {
+  //   sessionStorage.setItem("token", JSON.stringify(token));
+  // }
 
   //The useEffect hook is then used to perform an action on the component mount
-  useEffect(() => {
-    //First it is checked if there is a token stored in the sessionStorage
-    if (sessionStorage.getItem("token")) {
-      //If there is a stored 'token', parse it and then update its state.
-      let data = JSON.parse(sessionStorage.getItem("token"));
-      setToken(data);
-    }
-  }, []);
+  // useEffect(() => {
+  //   //First it is checked if there is a token stored in the sessionStorage
+  //   if (sessionStorage.getItem("token")) {
+  //     //If there is a stored 'token', parse it and then update its state.
+  //     let data = JSON.parse(sessionStorage.getItem("token"));
+  //     setToken(data);
+  //   }
+  // }, []);
 
   /**
    * The token is the parsed to the sign in page as a prop
    */
   return (
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="SignUp" element={<SignUp />} />
-          <Route path="/" element={<SignIn setToken={setToken} />} />
-          <Route path="Home" element={<Home />} />
-          {/* <Route path="/Favorites:userId" element={<Favorites />}/> */}
+          <Route path="SignIn" element={<SignIn setToken={setToken} />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Favorites:userId" element={<Favorites />}/>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <RouterProvider router={router}/>
     </>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import SignIn from "./SignIn";
+import { Link } from "react-router-dom"
 
 /**
  * The Hero component is a React functional component that renders a fixed position logo and
  * the logo name "Podify".
  */
 const Hero = () => {
+  const [ showSignIn, setShowSignIn ] = useState(false);
+
+  const signin = () => {
+    console.log('this button registers')
+    setShowSignIn(true)
+  }
   return (
     <>
-      <div style={{ position: "fixed" }}>
+      <div className="hero">
         <a href="index.html">
           <div
             style={{
@@ -18,11 +26,16 @@ const Hero = () => {
               alignItems: "center",
               justifyContent: "center",
               backgroundColor: "#F1FDFC",
+              marginRight: '1rem'
             }}
           >
             <img className="logo" src="podcast.png" alt="logo-image" />
           </div>
         </a>
+        <div>
+          
+          <Link className="sign-in-button" to='/SignIn'>Sign In</Link>
+        </div>
       </div>
       <div className="logo-name">
         <h1>Podify</h1>
